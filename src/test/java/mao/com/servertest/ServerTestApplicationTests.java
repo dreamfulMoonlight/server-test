02c52189641526dcf88822628ac31a6798c13fe8
maoning <980913140@qq.com>
2020-10-06 20:40:08 +0800

    Initial commit

---
+++ src/test/java/mao/com/servertest/ServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package mao.com.servertest;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
